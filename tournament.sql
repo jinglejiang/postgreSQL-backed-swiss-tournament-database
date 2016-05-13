021a10216c13bfde012ce42c0bef4c20a7574b9b
jinglejiang <jxj250@case.edu>
2016-05-12 22:07:01 -0400

    init

---
+++ tournament.sql
@@ -0,0 +1,24 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+
+DROP DATABASE IF EXISTS tournamentdb;
+
+CREATE DATABASE tournamentdb;
+
+\connect tournamentdb;
+
+CREATE TABLE players(
+	ID SERIAL primary key,
+	name TEXT);
+
+CREATE TABLE matches(
+	matchID SERIAL,
+	winner INTEGER REFERENCES players (ID),	
+	loser INTEGER REFERENCES players (ID));
